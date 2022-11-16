25b10bbb896a6c85f6e106a623208c68807f5471
Wilson Chan <wilsonchan54@gmail.com>
2022-11-15 23:38:34 -0500

    Added useState to change background color of line chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, LineSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 
 const LineChart = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
     id="line-chart"
     height= "500px"
     primaryXAxis={LinePrimaryXAxis}
     primaryYAxis={LinePrimaryYAxis}
-    chartArea={{border: {width:0 }}}
-    tooltip={{ enable: true}}
+    chartArea={{border: { width:0 }}}
+    tooltip={{ enable: true }}
+    background={currentMode === 'Dark' ? '#0c0c0c' : '#fff'}
     >
       <Inject services={[DateTime, Legend, LineSeries, Tooltip]} />
       <SeriesCollectionDirective>
